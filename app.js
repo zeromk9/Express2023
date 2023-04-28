cacde7c388b1419ec0526fa6e0a1bb3034982178
Martin David Corral Castro <zeromarthk@hotmail.com>
2023-04-28 09:28:11 -0600

    C2: Manejo de Rutas

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('Hola Mundo!');
+    res.send('Una API basica desde Express!');
+});
+
+app.get('/saludo', function (req, res) {
+    res.send('Hola desde la API!');
+});
+
+app.get('/despedida', function (req, res) {
+    res.send('Adios desde una API!');
 });
 
 app.listen(3000, function () {
