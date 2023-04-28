0a02cf0db34f1c2237d47e466ca245979bce7102
Martin David Corral Castro <zeromarthk@hotmail.com>
2023-04-28 09:50:04 -0600

    C4: Servidor con bodyParser

--- app.js
+++ app.js
@@ -16,3 +16,27 @@ app.get('/despedida', function (req, res) {
 app.listen(3000, function () {
     console.log('Aplicacion ejemplo, escuchando el puerto 3000!');
 });
+
+
+//Require packages and set the port
+const express = require('express');
+const prot = 3002;
+//para permitir manejo de POST y PUT
+const dobyParser = require('body-parser');
+const routes = require('./routes/routes');
+const app = express();
+
+//Usar Node.js body parsing middeware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true,
+}));
+
+routes(app);
+
+//Iniciar el servidor
+const server = app.listen(port,(error) => {
+    if (error) return console.log('Error: $(error)');
+
+    console.log('El servidor escucha en el puerto ${(server.address().port}');
+});
